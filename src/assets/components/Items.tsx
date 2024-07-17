cc57fe3af5c099523af4975087304db0becc8ac1
cdricn <cdricncdricn@gmail.com>
2024-07-17 21:46:48 +0800

    Added main design

---
+++ src/assets/components/Items.tsx
@@ -0,0 +1,22 @@
+import './Items.css'
+
+function Items() {
+  return (
+    <>
+    <div className='content'>
+      <div className='item-container'></div>
+      <div className='item-container'></div>
+      <div className='item-container'></div>
+      <div className='item-container'></div>
+      <div className='item-container'></div>
+      <div className='item-container'></div>
+      <div className='item-container'></div>
+      <div className='item-container'></div>
+      <div className='item-container'></div>
+      <div className='item-container'></div>
+    </div>
+    </>
+  )
+}
+
+export default Items
