2f28e6545ade8b7ed44f1c226e5d7a6760f1ceb4
cdricn <cdricncdricn@gmail.com>
2024-07-23 23:23:26 +0800

    Testing for horizontal scroll

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import './App.css'
 import './fonts.css'
 import icon from './assets/githubicon.png'
 import Items from './assets/components/Items.tsx'
+import { useRef } from 'react';
 
 function App() {
+  const scrollableDivRef = useRef<HTMLDivElement>(null);
+
+  const handleScrollDown = () => {
+    const div = scrollableDivRef.current;
+    if (div) {
+      div.scrollLeft += 10; // Scroll down by 10 pixels
+    }
+  };
 
   return (
     <>
@@ -11,7 +20,7 @@ function App() {
         <div className='page-header'>
           CDRICN
           <a
-            //href="https://github.com/cdricn"
+            href="https://github.com/cdricn"
             target="_blank">
             <img 
               src={icon}
@@ -19,13 +28,13 @@ function App() {
               alt="GitHub account link icon"
               >
             </img>
-          </a>
-        </div>
+          </a><button onClick={handleScrollDown}>Scroll Down</button>
+        </div> 
         <div className='itemview'>
           <div className='arrow-left'></div>
-          <ul id='scroll-container' className='content'>
+          <div className='content' ref={scrollableDivRef}>
             <Items />
-          </ul>
+          </div>
           <div className='arrow-right'></div>
         </div>
       </div>
--- src/assets/components/Items.tsx
+++ src/assets/components/Items.tsx
@@ -3,10 +3,34 @@ import linkImg from '../../assets/arrowdark.png'
 import data from '../../../src/pagecontent.json'
 
 function status(item:any){
-  if (item.body === ""){
+  return item.status === "done" ? "(Finished)" : "(In Progress)";
+}
+
+function contentVisible(item:any){
+  if(item.header === ""){
     return "";
   }else{
-    return item.status === "done" ? "(Finished)" : "(In Progress)";
+    return (
+      <>
+        <h1 className='content-header'>
+          {item.header}
+          <span className='content-status'>
+            {status(item)}
+          </span>
+        </h1>
+        <p className='content-body'>
+          {item.body}
+          <span className='content-link'>
+            <a href={item.link} target="_blank">
+              <img src={linkImg} alt="Link"></img>
+            </a>
+          </span>
+        </p>
+        <div className="content-image">
+          {item.image}
+        </div>
+      </>
+    );  
   }
 }
 
@@ -21,23 +45,7 @@ function Items() {
               <div className='title-overlay'>{item.title}</div>
             </div>
             <div className='content-container'>
-            <h1 className='content-header'>
-              {item.header}
-              <span className='content-status'>
-                {status(item)}
-              </span>
-            </h1>
-            <p className='content-body'>
-              {item.body}
-              <span className='content-link'>
-                <a href={item.link} target="_blank">
-                  <img src={linkImg} alt="Link"></img>
-                </a>
-              </span>
-            </p>
-            <div className="content-image">
-              {item.image}
-            </div>
+              {contentVisible(item)}
             </div>
           </li>
         </div>
