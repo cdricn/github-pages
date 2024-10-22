b72cc732f870a8a4c44a2738bfc7aa09ecec5b18
cdricn <cdricncdricn@gmail.com>
2024-10-23 02:16:17 +0800

    Updated design

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,30 @@
 import './App.css'
 import './fonts.css'
+import Item from './assets/components/Item.tsx'
 import icon from './assets/githubicon.png'
-import Items from './assets/components/Items.tsx'
-import { useRef } from 'react';
 
 function App() {
-  const scrollableDivRef = useRef<HTMLDivElement>(null);
-
-  const handleScrollRight = () => {
-    const div = scrollableDivRef.current;
-    if (div) {
-      div.scrollLeft += 2* 100; // 11vw
-    }
-  };
-  const handleScrollLeft = () => {
-    const div = scrollableDivRef.current;
-    if (div) {
-      div.scrollLeft -= 2 * 100; // Scroll down by 10 pixels
-    }
-  };
-
-  const handleWheel = (event:any) => {
-    if(event.deltaY > 0){
-      handleScrollRight();
-    }else{
-      handleScrollLeft();
-    }
-    // Positive for up-right, negative for down-left
-    console.log('Wheel event:', event.deltaY);
-  };
 
   return (
     <>
-      <div className='main-container' onWheel={handleWheel}>
-        <div className='page-header'>
-          CDRICN
-          <a
-            href="https://github.com/cdricn"
-            target="_blank">
-            <img 
-              src={icon}
-              className="gh-icon"
-              alt="GitHub account link icon"
-            >
-            </img>
+      <div className='header'>
+        <h1>CDRICN</h1>
+        <span>
+          <a href='https://github.com/cdricn'>
+            <img src={icon} className='test'></img>
           </a>
-        </div> 
-        <div className='itemview'>
-          <div className='arrow-left' onClick={handleScrollLeft}>
-          </div>
-          <div className='content' ref={scrollableDivRef}>
-            <Items />
-          </div>
-          <div className='arrow-right' onClick={handleScrollRight}>
-          </div>
-        </div>
+        </span>
+      </div>
+      <div className='menu-container'>
+        <h2>Projects</h2>
+        <h2>About</h2>
+        <h2>Top</h2>
+      </div>
+      <div className='items-container'>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
       </div>
     </>
   );
---
+++ src/assets/components/Item.tsx
@@ -0,0 +1,14 @@
+import './Item.css'
+
+function Item() {
+  return (
+    <div className='item-box'>
+      <div className='item-number'>01</div>
+      <div className='item-description'>
+
+      </div>
+    </div>
+  );
+}
+
+export default Item
