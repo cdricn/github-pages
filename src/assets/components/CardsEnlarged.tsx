ef9ab334fdd3fb3befb43ad3824803fc0ee6eaac
cdricn <cdricncdricn@gmail.com>
2024-12-03 22:52:38 +0800

    Fixed lines and added entry

--- src/assets/components/CardsEnlarged.tsx
+++ src/assets/components/CardsEnlarged.tsx
@@ -6,19 +6,25 @@ function CardEnlarged({ tags, header, details, content, link, link2, image, hand
   return(
     <section className='overlay-popup-container'>
       <article className='popup-card'>
-        <span>{tags}</span>
-        <h1>{header}</h1>
-        <div className='popup-body'>
+        <section className='popup-body'>
+          <span>{tags}</span>
+          <h1>{header}</h1>
           <p>{details} {content}</p>
+        </section>
+        <section className='popup-links'>
           <h2>Links</h2>
           <p>{link[0]}: <a href={link[1]} target='_blank'>{link[1]}</a></p>
           {
             link2![0] == null ? null : 
             <p>{link2![0]}: <a href={link2![1]} target='_blank'>{link2![1]}</a></p>
           }
+        </section>
+        <section className='popup-gallery'>
           <h2>Gallery</h2>
-          <div>{image}</div>
-        </div>
+          {
+            image != "" ? <img src={image}></img>: <p>No Image</p>
+          }
+        </section>
       </article>
       <div className="shadow-overlay" onClick={handlePopUp}>
       </div>
