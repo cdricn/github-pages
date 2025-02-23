52b597c77847159dd2ceab8055d7c8d8a327de5a
cdricn <cdricncdricn@gmail.com>
2025-02-23 16:20:30 +0800

    Deleted old files

--- src/assets/components/CardsEnlarged.tsx
+++ src/assets/components/CardsEnlarged.tsx
@@ -1,34 +1,28 @@
 import './CardsEnlarged.css'
-import { DataPropsWithState } from '../lib/interface'
 
-function CardEnlarged({ tags, header, details, content, link, link2, image, handlePopUp } : DataPropsWithState) {
+function CardEnlarged() {
 
   return(
     <section className='overlay-popup-container'>
       <article className='popup-card'>
-        <div className='close-btn-container' onClick={handlePopUp}>
+        <div className='close-btn-container'>
         </div>
         <section className='popup-body'>
-          <span>{tags}</span>
-          <h1>{header}</h1>
-          <p>{details} {content}</p>
+          <span>{}</span>
+          <h1>{}</h1>
+          <p>{} {}</p>
         </section>
         <section className='popup-links'>
           <h2>Links</h2>
-          <p>{link[0]}: <a href={link[1]} target='_blank'>{link[1]}</a></p>
-          {
-            link2![0] == null ? null : 
-            <p>{link2![0]}: <a href={link2![1]} target='_blank'>{link2![1]}</a></p>
-          }
+          <p>: <a href='' target='_blank'></a></p>
         </section>
         <section className='popup-gallery'>
           <h2>Gallery</h2>
           {
-            image != "" ? <img src={image}></img>: <p>No Image</p>
           }
         </section>
       </article>
-      <div className="shadow-overlay" onClick={handlePopUp}>
+      <div className="shadow-overlay">
       </div>
     </section>
   );
