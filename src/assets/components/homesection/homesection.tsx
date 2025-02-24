bf9c8732cb2e65b939bf4838ebead59126ead403
cdricn <cdricncdricn@gmail.com>
2025-02-24 17:38:41 +0800

    Fixed responsive font sizing

--- src/assets/components/homesection/homesection.tsx
+++ src/assets/components/homesection/homesection.tsx
@@ -7,22 +7,25 @@ export default function HomeSection() {
   return(
     <section className={styles['section']}>
       <div className={styles['home-intro-container']}>
-        <h1 className={styles['home-header']}>
+        <div className={styles['home-header']}>
           <span style={animDelay(.4, .3)}>Hello,</span><br/>
           <span style={animDelay(.8, .3)}>I'm </span>
           <span style={animDelay(1.2, .3)}>Adrian!</span>
-        </h1>
+        </div>
         <p className={styles['home-about']}>I’m an aspiring software developer residing 
           in the Philippines. I enjoy creating websites, from thinking of the design
-          to coding and deploying it!<br/><br/></p>
+          to coding and deploying it!<br/><br/>
+        </p>
       </div>
       <div className={styles['links-container']}>
         <div className={styles['links-column-left']}>
+          <LinkCircle url="" img="" delay={3}/>
           <LinkCircle url="" img="" delay={1}/>
           <LinkCircle url="https://github.com/cdricn" img="github_circle.png" alt="github"
                                     delay={3}/>
           <LinkCircle url="https://www.linkedin.com/in/adrian-cortezano-95b749254/" img="linkedin_circle.png" alt="linkedin"
                                     delay={5}/>
+          <LinkCircle url="" img="" delay={2}/>
           <LinkCircle url="" img="" delay={2}/>
         </div>
         <div className={styles['links-column-right']}>
@@ -31,6 +34,7 @@ export default function HomeSection() {
           <LinkCircle url="https://cdricn.itch.io/" img="itch_circle.png" alt="itch"
                                     delay={4}/>
           <LinkCircle url="" img="" delay={3}/>
+          <LinkCircle url="" img="" delay={1}/>
         </div>
       </div>
     </section>
