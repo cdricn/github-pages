e9d064c09b3e7e17eab7e4b5a8286336d84228e3
Adrian <110372020+cdricn@users.noreply.github.com>
2025-02-25 13:56:26 +0800

    Update projectcard.tsx

--- src/assets/components/projectsection/projectcard.tsx
+++ src/assets/components/projectsection/projectcard.tsx
@@ -3,7 +3,7 @@ import { DataProps } from '../../lib/interface';
 
 export default function ProjectCard({title, details, link, number} : DataProps) {
   return(
-    <a className={styles['card-container']} href={link}>
+    <a className={styles['card-container']} href={link} target="_blank">
       <div className={styles['card-content']}>
         <h3 className={styles['card-title']}>
           {title}
