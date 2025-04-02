c852b52162cf192c02543618def6c7661d95dd2f
cdricn <cdricncdricn@gmail.com>
2025-04-02 20:34:39 +0800

    Reedited & renamed json file structures

--- src/assets/lib/interface.ts
+++ src/assets/lib/interface.ts
@@ -1,21 +1,14 @@
-export interface LinkCircleProps {
-  url: string;
-  img: string;
-  alt?: string;
-  delay: number;
-}
-
-export interface CardProps {
-  cardCount: string;
-  cardTags?: string;
-  cardTitle: string;
-  cardDetails: string;
-}
-
 export interface DataProps {
-  /*tags: string;*/
-  number: string;
-  title?: string;
-  details?: string;
+  tags: string;
+  number?: string;
+  title: string;
+  details: string;
   link: string;
+  image: string;
+}
+
+export interface ProjectSectionProps {
+  header: string;
+  subtitle: string;
+  items: DataProps[];
 }
