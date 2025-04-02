c52b3f41a71617d9ad046ee1c8518ca5859ad843
cdricn <cdricncdricn@gmail.com>
2025-04-02 21:05:09 +0800

    Added icons and links for landing section

--- src/assets/components/ui/homesection/homesection.tsx
+++ src/assets/components/ui/homesection/homesection.tsx
@@ -1,4 +1,6 @@
 import styles from './homesection.module.css'
+import { SiItchdotio, SiGithub, SiLinkedin } from "react-icons/si";
+
 
 export default function HomeSection() {
 
@@ -9,11 +11,35 @@ export default function HomeSection() {
           Hello, I'm Adrian
         </h1>
         <div className={styles['links']}>
-          <a href='' target='_blank' className={styles['link-container']}>
+          <a href='https://github.com/cdricn' 
+            target='_blank' 
+            className={styles['link-container']}
+            style={{backgroundColor: 'var(--color-dark)'}}
+          >
+            <SiGithub 
+              className={styles['link-icon']}
+              style={{color: 'var(--color-white)'}}
+            />
           </a>
-          <a href='' target='_blank' className={styles['link-container']}>
+          <a href='https://www.linkedin.com/in/adrian-mcortezano/' 
+            target='_blank' 
+            className={styles['link-container']}
+            style={{backgroundColor: 'var(--color-deepblue)'}}
+          >
+            <SiLinkedin 
+              className={styles['link-icon']}
+              style={{color: 'var(--color-white)'}}
+            />
           </a>
-          <a href='' target='_blank' className={styles['link-container']}>
+          <a href='https://cdricn.itch.io/' 
+            target='_blank' 
+            className={styles['link-container']}
+            style={{backgroundColor: 'var(--color-deepred)'}}
+          >
+            <SiItchdotio 
+              className={styles['link-icon']}
+              style={{color: 'var(--color-white)'}}
+            />
           </a>
         </div>
       </div>
