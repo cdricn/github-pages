2b855d15284642539647bef9892a8821b5c94626
cdricn <cdricncdricn@gmail.com>
2026-02-02 21:31:52 +0800

    Updated imports and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
 import './fonts.css';
-import HomeSection from './assets/components/ui/homesection/homesection';
-import AboutSection from './assets/components/ui/aboutsection/aboutsection';
-import ProjectSection from './assets/components/ui/projectsection/projectsection';
-import Divider from './assets/components/ui/divider';
-import Navbar from './assets/components/ui/navbar/navbar';
-import Cursor from './assets/components/ui/cursor/cursor';
-import projectsData from './projects.json'
-import othersData from './others.json'
-
+import Project from './projectsection/project';
+import Cursor from './components/cursor/cursor';
+import Landing from './landing/landing';
 
 export default function App() {
 
   return (
     <>
-      <Cursor/>
-      <Navbar />
-      <HomeSection />
-      <Divider color={'--color-purple'}/>
-      <AboutSection />
-      <Divider color={'--color-cyan'}/>
-      <ProjectSection 
-        id='project'
-        header='Projects'
-        subtitle='Front-end development projects.'
-        items={projectsData}
-      />
-      <Divider color={'--color-green'}/>
-      <ProjectSection 
-        id='others'
-        header='Others'
-        subtitle='A collection of miscellaneous projects I’ve created or was part in.'
-        items={othersData}
-      />
-      <Divider color={'--color-gray'}/>
+      <Project />
     </>
   );
 }
