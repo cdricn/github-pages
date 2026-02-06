8d0009724f561155c85413e218feeeec07ce5923
cdricn <cdricncdricn@gmail.com>
2026-02-06 21:40:00 +0800

    Added imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import './fonts.css';
 import Project from './projectsection/project';
 import Cursor from './components/cursor/cursor';
 import Landing from './landing/landing';
+import Techstack from './components/techstack/techstack';
 
 export default function App() {
 
   return (
     <>
+      <Cursor />
+      <Landing />
+      <Techstack />
       <Project />
     </>
   );
