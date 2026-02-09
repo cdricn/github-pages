a943ef0a6737330325df326a73f339b5b4a200da
cdricn <cdricncdricn@gmail.com>
2026-02-09 16:16:40 +0800

    Fixed layout

--- src/components/techstack/techstack.tsx
+++ src/components/techstack/techstack.tsx
@@ -5,15 +5,37 @@ export default function Techstack() {
 
   return (
     <section className={styles['section']}>
-      <div className={styles['logo-html']}>
-        <SiHtml5 />
-        <span className={styles['hover-text']}>HTML</span>
+      <div className={styles['section-container']}>
+        <h2 className={styles['section-title']}>Techstack</h2>
+
+        <div className={styles['section-tech']}>
+          <div className={styles['logo']}>
+            <div className={styles['logo-html']}><SiHtml5 /></div>
+            <span className={styles['hover-text']}>HTML</span>
+          </div>
+          <div className={styles['logo']}>
+            <div className={styles['logo-js']}><SiJavascript /></div>
+            <span className={styles['hover-text']}>JavaScript</span>
+            </div>
+          <div className={styles['logo']}>
+            <div className={styles['logo-css']}><SiCss3 /></div>
+            <span className={styles['hover-text']}>CSS</span>
+          </div>
+          <div className={styles['logo']}>
+            <div className={styles['logo-ts']}><SiTypescript /></div>
+            <span className={styles['hover-text']}>TypeScript</span>
+          </div>
+          <div className={styles['logo']}>
+            <div className={styles['logo-react']}><SiReact /></div>
+            <span className={styles['hover-text']}>React</span>
+          </div>
+          <div className={styles['logo']}>
+            <div className={styles['logo-nextjs']}><SiNextdotjs /></div>
+            <span className={styles['hover-text']}>NextJS</span>
+          </div>
+        </div>
+
       </div>
-      <div className={styles['logo-js']}><SiJavascript /></div>
-      <div className={styles['logo-css']}><SiCss3 /></div>
-      <div className={styles['logo-ts']}><SiTypescript /></div>
-      <div className={styles['logo-nextjs']}><SiNextdotjs /></div>
-      <div className={styles['logo-react']}><SiReact /></div>
     </section>
   )
 }
