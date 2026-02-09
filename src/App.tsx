6913d54554a6d67d3e4f22b133d8338f31c0a715
cdricn <cdricncdricn@gmail.com>
2026-02-09 16:17:01 +0800

    Added test grid ruler

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Project from './projectsection/project';
 import Cursor from './components/cursor/cursor';
 import Landing from './landing/landing';
 import Techstack from './components/techstack/techstack';
+import GridTest from './components/test/GridTest';
 
 export default function App() {
 
   return (
     <>
+      <GridTest />
       <Cursor />
       <Landing />
       <Techstack />
---
+++ src/components/test/GridTest.tsx
@@ -0,0 +1,21 @@
+export default function GridTest() {
+
+  return (
+    <div className='gridtest-container'>
+      <div className='gridtest'>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+      </div>
+    </div>
+  )
+}
