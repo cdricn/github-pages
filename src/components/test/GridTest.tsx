b6bcc96200301d60e37789144c9c2599dd75ca2e
cdricn <cdricncdricn@gmail.com>
2026-02-09 18:14:29 +0800

    Grid design test

--- src/components/test/GridTest.tsx
+++ src/components/test/GridTest.tsx
@@ -3,18 +3,18 @@ export default function GridTest() {
   return (
     <div className='gridtest-container'>
       <div className='gridtest'>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
-        <div style={{backgroundColor: 'gray', opacity:.1}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
+        <div style={{borderTop:'none', border: '1px solid gray', opacity:.2}}></div>
       </div>
     </div>
   )
