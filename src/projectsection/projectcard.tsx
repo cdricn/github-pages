08fd933ec19df1d2ee793c1a285760c932c82225
cdricn <cdricncdricn@gmail.com>
2026-02-09 15:49:39 +0800

    Fixed styling

--- src/projectsection/projectcard.tsx
+++ src/projectsection/projectcard.tsx
@@ -6,37 +6,28 @@ import { SlLink } from "react-icons/sl";
 export default function ProjectCard({tags, title, details, link, github_link, index, image} : ProjectCardInfo) {
   let cardIndex = index < 10 ? 
    "0"+(index+1).toString() : (index+1).toString()
-                  
-  const linkImage:React.CSSProperties = {
-    backgroundImage: `url(${image})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center'
-  }
   
   return(
     <div className={styles['card-container']}>
-      <div className={styles['card-content']}>
-        <h3 className={styles['card-header']}>
-          {title}
-          {link ? <a href={link}><SlLink /></a>: <></>}
-          {
-            github_link ? 
-              <a href={github_link} className={styles['github-logo']}>
-                <DiGithubBadge />
-              </a> : <></>
-          }
-        </h3>
-        <div className={styles['card-tags']}>
-          {tags}
-        </div>
-        <p className={styles['card-description']}>
-          {details}
-        </p>
-        <div className={styles['card-number']}>
-          {cardIndex}
-        </div>
+      <h3 className={styles['card-header']}>
+        {title}
+        <span className={styles['card-number']}>{cardIndex}</span>
+      </h3>
+      <span className={styles['card-links']}>{link ? <a href={link}><SlLink /></a>: <></>}</span>
+      <span className={styles['card-links']}>
+        {
+          github_link ? 
+            <a href={github_link} className={styles['github-logo']}>
+              <DiGithubBadge />
+            </a> : <></>
+        }
+      </span>
+      <div className={styles['card-tags']}>
+        {tags}
       </div>
+      <p className={styles['card-description']}>
+        {details}
+      </p>
     </div>
   );
 }
